89a375a342f724b6ed1061bc8ef386278f18dbd3
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-05-25 21:52:29 -0300

    mucha coisa

--- Inicio/index.php
+++ Inicio/index.php
@@ -21,6 +21,8 @@ function confirmacao(id) {
 </script>
 
 
+</script>
+
 </head>
 
  <body>
@@ -53,29 +55,32 @@ require_once "../conecta.php";
 
 echo '<table id = "documentos" class = "highlight " border = 2>';
 
-echo "<tr> <th> Nome do Documento </th> <th> Forma </th> <th> Formato </th> <th> Especie </th>  <th colspan = 5> Operações </th> </tr> <br>";
+echo "<tr> <th> Imagem </th><th> Nome do Documento </th> <th> Forma </th> <th> Formato </th> <th> Especie </th>  <th colspan = 4> Operações </th> </tr> <br>";
 
 //Realizando o comando select para puxar os documentos do Banco de dados
 
-$sql = "SELECT `id`, `titulo`, `forma`, `formato`, `especie` FROM `documentos`";
+$sql = "SELECT `id`, `titulo`, `forma`, `formato`, `especie`, `imagem`FROM `documentos`";
 
 $resultado = mysqli_query($conexao, $sql);
 
 //Criando o array com todos os documentos
 
-while($documentos = mysqli_fetch_array($resultado, MYSQLI_BOTH)){
+$documentos = mysqli_fetch_all($resultado, MYSQLI_BOTH);
 
+
+     foreach($documentos as $chave => $documento){
      echo "<tr>";
+     echo "<td> <img width = 250 src = ../upload/" . $documento['imagem'] . "></td>";
+     echo "<td>" . $documento['titulo']. "</td>";
+     echo "<td>" . $documento['forma'] . "</td>";
+     echo "<td>" . $documento['formato'] . "</td>";
+     echo "<td>" . $documento['especie'] . "</td>";
      
-     echo "<td>" . $documentos['titulo']. "</td>";
-     echo "<td>" . $documentos['forma'] . "</td>";
-     echo "<td>" . $documentos['formato'] . "</td>";
-     echo "<td>" . $documentos['especie'] . "</td>";
    
    
-     echo "<td class ='waves-effect waves-teal btn-flat'> <a href = '../Documentos/vermais.php?id=$documentos[id]'> Ver mais </a>";
-     echo "<td class ='waves-effect waves-teal btn-flat'> <a href= '../Documentos/formaltera.php?id=$documentos[id]'> Editar </a>";
-     echo "<td class ='waves-effect waves-teal btn-flat'> <a href='#'" . "onclick='confirmacao($documentos[id])'>" . "Excluir </a>" ;
+     echo "<td class ='waves-effect waves-teal btn-flat'> <a href = '../Documentos/vermais.php?id=$documento[id]'> Ver mais </a>";
+     echo "<td class ='waves-effect waves-teal btn-flat'> <a href= '../Documentos/formaltera.php?id=$documento[id]'> Editar </a>";
+     echo "<td class ='waves-effect waves-teal btn-flat'> <a href='#'" . "onclick='confirmacao($documento[id])'>" . "Excluir </a>" ;
      echo "<a href='inicio.html' class ='voltar'>";
      echo "</tr>";
    
