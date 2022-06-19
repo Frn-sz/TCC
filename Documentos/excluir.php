0b4c504b682fad27abdb76ea5838fed467d36728
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-06-19 17:21:29 -0300

    arrumando o excluir

--- Documentos/excluir.php
+++ Documentos/excluir.php
@@ -6,9 +6,13 @@ $id = $_GET['id'];
 $sql = "DELETE FROM documentos WHERE id=$id";
 $resultado = mysqli_query($conexao,$sql);
 
+
+$sql2 = "DELETE FROM tabela_assoc WHERE id_doc=$id";
+$resultado2 = mysqli_query($conexao,$sql2);
+
 mysqli_close($conexao);
 
-if($resultado){
+if($resultado and $resultado2){
     header("Location:../Inicio/index.php");
 }
 ?>
