053afd5a06b83074adb365653f75adc1ffdb0d86
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-07-25 14:02:08 -0300

    sdssa

--- Documentos/removerfavorito.php
+++ Documentos/removerfavorito.php
@@ -9,9 +9,7 @@ $url = $_SERVER['HTTP_REFERER'];
 $id_documento = $_GET['id'];
 $id_usuario = $_SESSION['id_usuario'];
 $sql = "DELETE FROM favoritos WHERE id_documento = '$id_documento' and id_usuario = '$id_usuario'";
-var_dump($sql);
 $result = mysqli_query($conexao, $sql);
-
 if($result){
     header("location: $url");
 }
