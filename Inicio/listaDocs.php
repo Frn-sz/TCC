83aba4264b6596455c24c359446629137914a129
Frn-sz <fernandogdemedeirosw@gmail.com>
2022-10-10 08:36:46 -0300

    fds mt

--- Inicio/listaDocs.php
+++ Inicio/listaDocs.php
@@ -5,20 +5,12 @@ if (!isset($_SESSION)) {
 ?>
 <title>Lista de Documentos</title>
 <style type="text/css">
-     .paginacao,
-     .paginacaoNum {
-          color: black;
-          background-color: white;
-          padding: 7px;
-          border-radius: 7px;
-     }
-
      .card-image {
-          height: 30vh !important;
+          height: 25vh !important;
           overflow: hidden !important;
           display: flex !important;
           align-items: flex-start !important;
-          padding: 10px;
+          padding: 2px;
 
      }
 
@@ -26,18 +18,76 @@ if (!isset($_SESSION)) {
           color: white !important;
      }
 
-     .imagem {
-          border-radius: px !important;
+     .collapsible {
+          color: black;
+          text-align: center;
+
+          cursor: pointer;
+     }
+
+     .topicosEncontrados {
+          color: white;
+          text-align: center;
+     }
+
+     .aTopicos,
+     .aDocumentos {
+          color: white !important;
+          padding: 10px;
+          font-size: 20px;
+          border-radius: 5px;
+     }
+
+     .imgCollapsible {
+          display: flex;
+     }
+
+     .collapsible {
+          border: none !important;
+     }
+
+     div.gallery {
+          margin: 5px;
+          border: 1px solid #ccc;
+          float: left;
+          width: 180px;
+     }
+
+     div.gallery:hover {
+          border: 1px solid #777;
+     }
+
+     div.gallery img {
+          width: 100%;
+          height: auto;
+     }
+
+     div.desc {
+          padding: 15px;
+          text-align: center;
+     }
+
+     .tituloTop {
+          color: white;
+          margin: auto;
+     }
+
+     .teste1 {
+          display: flex;
+          flex-direction: column;
      }
 </style>
 <main>
      <?php
      require_once "../interfaces/header.php";
      ?>
-
      <?php
      require_once "../conecta.php";
-
+     if (isset($_GET['escolha'])) {
+          $escolha  = $_GET['escolha'];
+     } else {
+          $escolha = "todos";
+     }
      //Definindo limite
      if (isset($_GET['pagina'])) {
           $pag = $_GET['pagina'];
@@ -46,7 +96,6 @@ if (!isset($_SESSION)) {
      }
      $limit = 8;
      $offset = $limit * ($pag - 1);
-
      if (isset($_SESSION['id_usuario'])) {
           if ($_SESSION['nvl_usuario'] == 1 or $_SESSION['nvl_usuario'] == 3) {
      ?>
@@ -61,18 +110,6 @@ if (!isset($_SESSION)) {
      //Verificando se é uma pesquisa ou a listagem de todos os documentos
      if (isset($_GET['busca'])) {
           $pesquisa = "%" . mysqli_real_escape_string($conexao, trim($_GET['busca'])) . "%";
-          // $Busca =
-          //      "SELECT DISTINCT idDoc FROM documentos AS F
-          // JOIN topicos AS D
-          // JOIN tabela_assoc AS T
-          // ON T.id_topico = D.idTop && F.idDoc = T.id_doc 
-          // WHERE D.tituloTop LIKE '$pesquisa' 
-          // OR F.tituloDoc LIKE '$pesquisa'
-          // OR F.transcricao LIKE '$pesquisa'
-          // OR F.palavrasChaves LIKE '$pesquisa'
-          // ORDER BY F.tituloDoc
-          // LIMIT $limit OFFSET $offset";
-          //Fazendo a busca por título, Palavras chaves e transcricao
           $BuscaDocumentos = "SELECT * FROM documentos AS D
           WHERE D.tituloDoc LIKE '$pesquisa'
           OR D.transcricao LIKE '$pesquisa'
@@ -86,12 +123,20 @@ if (!isset($_SESSION)) {
                          <h3 class="white-text">Nenhum documento encontrado</h3>
                     </div>
                </div>
-     <?php }
+          <?php }
           $ultimaPag = ceil($numRows / $limit);
           $pegandoTopicos = "SELECT * FROM topicos AS T WHERE T.tituloTop LIKE '$pesquisa'";
           $resultTopicos = mysqli_query($conexao, $pegandoTopicos);
           $topicos = mysqli_fetch_all($resultTopicos, MYSQLI_ASSOC);
-          var_dump($topicos);
+          foreach ($topicos as $topico) {
+               $selectDocTopicos = "SELECT * FROM documentos AS D
+                JOIN tabela_assoc AS t
+                ON D.idDoc = t.id_doc
+                WHERE t.id_topico = '$topico[idTop]'";
+               $query = mysqli_query($conexao, $selectDocTopicos);
+               $topicoName = $topico['tituloTop'];
+               $docTopico[$topicoName] = mysqli_fetch_all($query, MYSQLI_ASSOC);
+          }
      } else {
           //Puxando todos os documentos do banco de dados
           $sqlTotal = "SELECT count(*) FROM `documentos` ORDER BY tituloDoc";
@@ -104,106 +149,143 @@ if (!isset($_SESSION)) {
      }
      $proximo = $pag + 1;
      $anterior = $pag - 1;
-     ?>
 
-     <div class='container'>
-          <div class='row'>
-               <?php
-               if (isset($documentos)) {
-                    foreach ($documentos as $chave => $documento) { ?>
-                         <a href="../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>">
-                              <div class="caixa">
-                                   <div class='col s6 m3'>
-                                        <div class='card hoverable'>
-                                             <div class='card-image cardindex'>
-                                                  <?php if ($documento['imagem'] != "") {  ?>
-                                                       <img class='imagem' src='../upload/<?= $documento['imagem'] ?>'>
-                                                  <?php } else { ?>
-                                                       <img class='imagem' src='../Imagens/placeholderSemImagem.png'>
-                                                  <?php } ?>
-                                             </div>
-                                             <div class='card-content'>
-                                                  <span class='card-title'><?= $documento['tituloDoc'] ?></span>
-                                                  <p> Forma:<?= $documento['forma'] ?> <br></p>
-                                                  <p> Formato:<?= $documento['formato'] ?> <br></p>
-                                                  <p> Espécie:<?= $documento['especie']  ?></p>
-                                             </div>
-                                             <div class='card-action center'>
+     if ($escolha != "todos") {
+          ?>
+          <div>
+               <div class="center">
+                    <a class="aTopicos" href="listaDocs.php?busca=<?= $_GET['busca'] ?>&&escolha=topicos">Tópicos</a>
+                    <a class="aDocumentos" href="listaDocs.php?busca=<?= $_GET['busca'] ?>&&escolha=documentos'">Documentos</a>
+               </div>
+               <br>
+          <?php }
+     if ($escolha != "topicos") {
+          ?>
+               <div class='container'>
+                    <div class='row'>
+                         <?php
+                         if (isset($documentos)) {
+                              foreach ($documentos as $chave => $documento) { ?>
+                                   <a href="../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>">
+                                        <div class="caixa">
+                                             <div class='col s6 m3'>
+                                                  <div class='card hoverable'>
+                                                       <div class='card-image cardindex'>
+                                                            <?php if ($documento['imagem'] != "") {  ?>
+                                                                 <img class='imagem' src='../upload/<?= $documento['imagem'] ?>'>
+                                                            <?php } else { ?>
+                                                                 <img class='imagem' src='../Imagens/placeholderSemImagem.png'>
+                                                            <?php } ?>
+                                                       </div>
+                                                       <div class='card-content'>
+                                                            <span class='card-title'><?= $documento['tituloDoc'] ?></span>
+                                                            <p> Forma:<?= $documento['forma'] ?> <br></p>
+                                                            <p> Formato:<?= $documento['formato'] ?> <br></p>
+                                                            <p> Espécie:<?= $documento['especie']  ?></p>
+                                                       </div>
+                                                       <div class='card-action center'>
 
-                                                  <?php if (!isset($_SESSION['id_usuario']) or $_SESSION['nvl_usuario'] == 2) { ?>
+                                                            <?php if (!isset($_SESSION['id_usuario']) or $_SESSION['nvl_usuario'] == 2) { ?>
 
-                                                       <a href='../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-large waves-effect waves-light white'><i class='material-icons black-text'>search</i> </a>
+                                                                 <a href='../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-large waves-effect waves-light white'><i class='material-icons black-text'>search</i> </a>
 
-                                                  <?php } else { ?>
+                                                            <?php } else { ?>
 
-                                                       <a href='../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-floating waves-effect waves-light white'><i class='material-icons black-text'>search</i> </a>
+                                                                 <a href='../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-floating waves-effect waves-light white'><i class='material-icons black-text'>search</i> </a>
 
-                                                  <?php } ?>
+                                                            <?php } ?>
 
-                                                  <?php if (isset($_SESSION['nvl_usuario'])) {
+                                                            <?php if (isset($_SESSION['nvl_usuario'])) {
 
-                                                       if ($_SESSION['nvl_usuario'] != 2) { ?>
-                                                            &nbsp
-                                                            <a href='../Documentos/formaltera.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-floating waves-effect waves-light  white'> <i class='material-icons black-text'>edit</i> </a>
-                                                            &nbsp
-                                                            <a class="waves-effect waves-light btn-floating modal-trigger white " href="#modal<?= $documento['idDoc'] ?>"><i class="material-icons black-text">delete</i></a>
-                                                  <?php }
-                                                  } ?>
-                         </a>
-          </div>
-     </div>
-     </div>
-     </div>
-     <div id="modal<?= $documento['idDoc'] ?>" class="modal">
-          <div class="modal-content">
-               <div class="row">
-                    <div class="center">
-                         <h4 class="black-text">Deseja mesmo excluir este documento?</h4>
+                                                                 if ($_SESSION['nvl_usuario'] != 2) { ?>
+                                                                      &nbsp
+                                                                      <a href='../Documentos/formaltera.php?idDoc=<?= $documento['idDoc'] ?>' class='btn-floating waves-effect waves-light  white'> <i class='material-icons black-text'>edit</i> </a>
+                                                                      &nbsp
+                                                                      <a class="waves-effect waves-light btn-floating modal-trigger white " href="#modal<?= $documento['idDoc'] ?>"><i class="material-icons black-text">delete</i></a>
+                                                            <?php }
+                                                            } ?>
+                                   </a>
                     </div>
                </div>
-               <form action="../Documentos/excluir.php" method="get">
+          </div>
+          <div id="modal<?= $documento['idDoc'] ?>" class="modal">
+               <div class="modal-content">
                     <div class="row">
                          <div class="center">
-                              <input type="hidden" name="idDoc" value="<?= $documento['idDoc']; ?>">
+                              <h4 class="black-text">Deseja mesmo excluir este documento?</h4>
                          </div>
                     </div>
-                    <div class="center">
-                         <button type="submit" class="btn waves-effect waves-green white black-text">Confirmar</button>
-                         <a href="#!" class="modal-close waves-effect waves-red white btn black-text">Cancelar</a>
-                    </div>
-               </form>
+                    <form action="../Documentos/excluir.php" method="get">
+                         <div class="row">
+                              <div class="center">
+                                   <input type="hidden" name="idDoc" value="<?= $documento['idDoc']; ?>">
+                              </div>
+                         </div>
+                         <div class="center">
+                              <button type="submit" class="btn waves-effect waves-green white black-text">Confirmar</button>
+                              <a href="#!" class="modal-close waves-effect waves-red white btn black-text">Cancelar</a>
+                         </div>
+                    </form>
+               </div>
           </div>
-     </div>
 <?php    }
-               } ?>
-</div>
-</div>
-</div>
+                         } ?>
 <div class="row">
-     <div class="center">
-          <ul class="pagination">
-               <?php if ($pag == 1) { ?>
-                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-               <?php } else { ?>
-                    <li class="waves-effect"><a href="listaDocs.php?pagina=<?= $anterior ?>"><i class="material-icons">chevron_left</i></a></li>
-               <?php }
-               for ($i = 1; $i <= $ultimaPag; $i++) { ?>
-                    <li class="active white "><a class="black-text" href="listaDocs.php?pagina=<?= $i ?>"><?= $i ?></a></li>
-               <?php }
-               if ($pag < $ultimaPag) { ?>
-                    <li class="waves-effect"><a href="listaDocs.php?pagina=<?= $proximo ?>"><i class="material-icons">chevron_right</i></a></li>
-               <?php } else { ?>
-                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-               <?php } ?>
-          </ul>
-     </div>
+
+     <ul class="pagination">
+          <?php if ($pag == 1) { ?>
+               <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <?php } else { ?>
+               <li class="waves-effect"><a href="listaDocs.php?pagina=<?= $anterior ?>"><i class="material-icons">chevron_left</i></a></li>
+          <?php }
+          for ($i = 1; $i <= $ultimaPag; $i++) { ?>
+               <li class="active white "><a class="black-text" href="listaDocs.php?pagina=<?= $i ?>"><?= $i ?></a></li>
+          <?php }
+          if ($pag < $ultimaPag) { ?>
+               <li class="waves-effect"><a href="listaDocs.php?pagina=<?= $proximo ?>"><i class="material-icons">chevron_right</i></a></li>
+          <?php } else { ?>
+               <li class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+          <?php } ?>
+     </ul>
+
 </div>
+<?php } else { ?>
+     <div class="teste1">
+          <?php
+          foreach ($topicos as $topico) { ?>
+               <div class="container teste">
+                    <h3 class="tituloTop"> <?= $topico['tituloTop'] ?> </h3>
+                    <ul>
+                         <?php foreach ($docTopico as $key => $doc) {
+                              foreach ($doc as $d) {
+                         ?>
+                                   <li>
+                                        <div class="gallery">
+                                             <div>
+                                                  <a href="../Documentos/vermais.php?idDoc=<?= $d['idDoc'] ?>"><img class="imgCollapsible" width=400 src="../upload/<?= $d['imagem'] ?>" alt=""></a>
+                                             </div>
+                                             <div class="desc">Add a description of the image here</div>
+                                        </div>
+                                   <li>
+                              <?php
+                              }
+                         } ?>
+                    </ul>
+               </div>
+          <?php } ?>
+
+     </div>
+<?php } ?>
 </main>
+
+<?php include_once "../interfaces/footer.php"; ?>
+<script>
+     $(document).ready(function() {
+          $('.collapsible').collapsible();
+     });
+</script>
 
 <script>
      $('.tabs').tabs('methodName');
      $('.tabs').tabs('methodName', paramName);
 </script>
-<?php include_once "../interfaces/footer.php"; ?>
-
-</html>
