f732e0f9723bb56a814088c977d5b4742b78a434
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-11-05 22:58:25 -0300

    fds

--- funcoes.php
+++ funcoes.php
@@ -81,40 +81,6 @@ function buscaTopicos($busca)
                 </div>
                 <div class="card-action">
                   <a href="../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>"><i class="material-icons">search</i></a>
-  $topicos = mysqli_fetch_all($query, MYSQLI_ASSOC); ?>
-  <div class="container">
-    <?php
-    foreach ($topicos as $topico) { ?>
-      <div class="center">
-        <h1 class="white-text"><?= $topico['tituloTop']; ?></h1>
-      </div>
-      <div class="row">
-        <?php $idTopico = $topico['idTop'];
-        $sql = "SELECT id_doc FROM tabela_assoc WHERE id_topico = '$idTopico'";
-        $query = mysqli_query($conexao, $sql);
-        $idDocs = mysqli_fetch_all($query, MYSQLI_ASSOC);
-        $documentos = array();
-        foreach ($idDocs as $idDoc) {
-          $idDoc = $idDoc['id_doc'];
-          $sql = "SELECT * FROM documentos WHERE idDoc = '$idDoc'";
-          $query = mysqli_query($conexao, $sql);
-          $documentos[] = mysqli_fetch_assoc($query);
-
-          foreach ($documentos as $documento) {
-        ?>
-            <a href="../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>">
-              <div class="col s14 m3 cardDocTopico">
-                <div class="card">
-                  <div class="card-image">
-                    <img class="imagemDocTopico hoverable" src="../upload/<?= $documento['imagem'] ?>">
-                    <span class="card-title"></span>
-                  </div>
-                  <div class="card-content">
-                    <h4><?= $documento['tituloDoc'] ?></h4>
-                  </div>
-                  <div class="card-action">
-                    <a href="../Documentos/vermais.php?idDoc=<?= $documento['idDoc'] ?>"><i class="material-icons">search</i></a>
-                  </div>
                 </div>
               </div>
             </div>
